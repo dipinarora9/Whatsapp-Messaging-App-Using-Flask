277ea1600375e4aa27a2077d7099d25898ef38d8
Dipin Arora <44615649+dipinarora9@users.noreply.github.com>
2018-12-30 14:10:07 +0530

    Update whatsappmsg.py

--- whatsappmsg.py
+++ whatsappmsg.py
@@ -17,10 +17,10 @@ num=0
 mes=''
 
 def send_message(num,mes): 
-    message = client.messages.create(from_='whatsapp:+14155238886', body='%s'%mes, to='whatsapp:+91%d'%num)
+    message = client.messages.create(from_='whatsapp:#add twilio number here', body='%s'%mes, to='whatsapp:+91%d'%num)
     return (message)
 
 def read_message(num):
-    message = client.messages.list(date_sent=(now.strftime("%Y, %m, %d, %H, %M")),from_='whatsapp:+91%d'%num, to='whatsapp:+14155238886')
+    message = client.messages.list(date_sent=(now.strftime("%Y, %m, %d, %H, %M")),from_='whatsapp:+91%d'%num, to='whatsapp:#add twilio number here')
     message_body=client.messages(message[0].sid).fetch()
     return (message_body.body)
