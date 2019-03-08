82ce15830acef4c3da30801d9c4b2cefe9d618d6
Dipin Arora <44615649+dipinarora9@users.noreply.github.com>
2019-03-08 15:33:40 +0530

    refactoring

--- mainserver.py
+++ mainserver.py
@@ -4,21 +4,18 @@ Created on 21-Oct-2018
 @author: Dipin Arora
 '''
 
-
-from flask import Flask, request, redirect,url_for, render_template, session,flash
+from flask import Flask, request, redirect, url_for, render_template, session, flash
 import gmail, whatsappmsg
 from functools import wraps
 from forms import LoginForm, RegisterForm, num_and_msg
 import sqlite3
 
-
-
-
-conn=sqlite3.connect("login.db")
-cur=conn.cursor()
+conn = sqlite3.connect("login.db")
+cur = conn.cursor()
 
 app = Flask(__name__)
-app.secret_key="dipin"
+app.secret_key = "dipin"
+
 
 def login_required(f):
     @wraps(f)
@@ -28,71 +25,60 @@ def login_required(f):
         else:
             flash("You need to LogIn First")
             return redirect("/")
+
     return wrap
 
 
-
-
-
 @app.route("/")
-def Welcome():
-    Car=None
-    return render_template("welcome.html",Car=Car)
+def welcome():
+    car = None
+    return render_template("welcome.html", car=car)
 
 
-
-
-
-@app.route("/login",methods=["GET","POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    conn=sqlite3.connect("login.db")
-    cur=conn.cursor()
-    error=None
-    form=LoginForm(request.form)
+    conn = sqlite3.connect("login.db")
+    cur = conn.cursor()
+    error = None
+    form = LoginForm(request.form)
     if request.method == "POST":
         user = cur.execute("SELECT username FROM login;")
-        username=user.fetchall()
-        passw=cur.execute("SELECT password FROM login")
-        password=passw.fetchall()
+        username = user.fetchall()
+        passw = cur.execute("SELECT password FROM login")
+        password = passw.fetchall()
         for i in username:
             for j in password:
-                if form.username.data==i[0] and form.password.data==j[0]:
-                    session["logged_in"]=True
+                if form.username.data == i[0] and form.password.data == j[0]:
+                    session["logged_in"] = True
                     flash("You were logged In!")
                     return redirect("/whatsapp")
                 else:
                     pass
 
         else:
-            error="Invalid Credentials. Please try again"
-               
+            error = "Invalid Credentials. Please try again"
+
     return render_template("login.html", form=form, error=error)
-
-
-
 
 
 @app.route('/logout')
 @login_required
 def logout():
-    session.pop("logged_in",None)
+    session.pop("logged_in", None)
     flash("You were logged Out!")
-    return redirect(url_for("Welcome"))
-    
+    return redirect(url_for("welcome"))
 
 
-
-
-
-@app.route("/register",methods=['GET','POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    conn=sqlite3.connect("login.db")
-    cur=conn.cursor()
+    conn = sqlite3.connect("login.db")
+    cur = conn.cursor()
     form = RegisterForm(request.form)
     if form.validate_on_submit():
-        gmail.mail(form.email.data,form.username.data)
+        gmail.mail(form.email.data, form.username.data)
 
-        cur.execute(""" INSERT INTO login VALUES (?,?,?,?)""",(form.username.data,form.email.data,form.password.data,form.number.data))
+        cur.execute(""" INSERT INTO login VALUES (?,?,?,?)""",
+                    (form.username.data, form.email.data, form.password.data, form.number.data))
 
         conn.commit()
         conn.close()
@@ -101,33 +87,22 @@ def register():
         return redirect('/login')
 
     return render_template('register.html', form=form)
-    
 
 
-
-       
-             
 @app.route("/whatsapp", methods=['GET', 'POST'])
 @login_required
 def whatsapp():
-    car=None
-    msg=''
-    form=num_and_msg(request.form)
-    if request.method=="POST":
-        msg=whatsappmsg.read_message(form.num.data)
+    car = None
+    msg = ''
+    form = num_and_msg(request.form)
+    if request.method == "POST":
+        msg = whatsappmsg.read_message(form.num.data)
         whatsappmsg.send_message(form.num.data, form.mes.data)
         print(form.mes)
     else:
         pass
-    return render_template("whatsappmsgs.html",form=form ,car=car, msg=msg)
-    
-
-   
-    
-
+    return render_template("whatsappmsgs.html", form=form, car=car, msg=msg)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
--- whatsappmsg.py
+++ whatsappmsg.py
@@ -4,23 +4,28 @@ Created on 21-Oct-2018
 @author: Dipin Arora
 '''
 
-from twilio.rest import Client 
+from twilio.rest import Client
 import datetime
 
-now=datetime.datetime.now()
+now = datetime.datetime.now()
 
-account_sid = ''   # add your account sid here
-auth_token = '' #add your authentication token here
+account_sid = ''  # add your account sid here
+auth_token = ''  # add your authentication token here
 client = Client(account_sid, auth_token)
 
-num=0
-mes=''
+num = 0
+mes = ''
 
-def send_message(num,mes): 
-    message = client.messages.create(from_='whatsapp:#add twilio number here', body='%s'%mes, to='whatsapp:+91%d'%num)
-    return (message)
+
+def send_message(num, mes):
+    message = client.messages.create(from_='whatsapp:#add twilio number here', body='%s' % mes,
+                                     to='whatsapp:+91%d' % num)
+    return message
+
 
 def read_message(num):
-    message = client.messages.list(date_sent=(now.strftime("%Y, %m, %d, %H, %M")),from_='whatsapp:+91%d'%num, to='whatsapp:#add twilio number here')
-    message_body=client.messages(message[0].sid).fetch()
-    return (message_body.body)
+    # message = client.messages.list(date_sent=(now.strftime("%Y, %m, %d, %H, %M")), from_='whatsapp:+91%d' % num,
+    #                                to='whatsapp:#add twilio number here')
+    message = client.messages.list(date_sent=datetime, )
+    message_body = client.messages(message[0].sid).fetch()
+    return message_body.body
